c0289f376c5503df6c38b6918c957de49cd6b350
ChloeQQQ <a0925010020@gmail.com>
2021-11-02 01:52:51 +0800

    update

---
+++ js/home.js
@@ -0,0 +1,13 @@
+
+// $(function(){ 
+
+
+// $('.txt-mid').mouseenter(function(e){
+//     $(this).find('.txt-mid-1').animate({'opacity':1});
+// });
+
+// $('.txt-mid').mouseout(function(e){
+//     $(this).find('.txt-mid-1').animate({'opacity':0});
+// });
+
+// });
